f8cc59a3eecb956a5f5b896a4b1ed271a26d2064
Lance Guyatt <lanceguyatt@gmail.com>
2023-05-30 07:32:28 +0200

    bump

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -3,20 +3,19 @@
   export { className as class }
   export let ghosted: boolean = false
   export let tag: string = 'a'
+  const BASE_CLASSES = 'inline-block h-6 px-2 appearance-none text-base leading-tight text-center whitespace-nowrap border-2'
 </script>
 
 <svelte:element
   this={tag}
   {...$$props}
-  class={`button ${className} ${ghosted && 'button--ghosted'}`}
+  class={`button ${BASE_CLASSES} ${className} ${ghosted && 'button--ghosted'}`}
 >
   <slot />
 </svelte:element>
 
 <style lang="postcss" is:global>
   .button {
-    @apply inline-block h-6 px-2 appearance-none text-base leading-tight text-center whitespace-nowrap border-2;
-
     border-image: url('/images/unselected.svg') 2 stretch;
 
     &:active {
