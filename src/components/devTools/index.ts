1bf56887a2c9a1334aa0fdfa5fb353b8a58d65aa
Lance Guyatt (lanceguyatt@gmail.com) <lanceguyatt@gmail.com>
2026-09-26 15:00:46 +0100

    remove unwanted

--- src/components/devTools/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as DevTools } from './DevTools.astro'
--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,6 +1,6 @@
-import focus from '@alpinejs/focus'
-import persist from '@alpinejs/persist'
-import ui from '@alpinejs/ui'
+// import focus from '@alpinejs/focus'
+// import persist from '@alpinejs/persist'
+// import ui from '@alpinejs/ui'
 import type { Alpine as AlpineType } from 'alpinejs'
 import AsyncAlpine from 'async-alpine'
 
@@ -8,7 +8,7 @@ import AsyncAlpine from 'async-alpine'
 // Plugin Setup
 // ============================================
 const setupPlugins = (Alpine: AlpineType) => {
-  ;[focus, ui, persist].forEach((plugin) => Alpine.plugin(plugin))
+  // ;[focus, ui, persist].forEach((plugin) => Alpine.plugin(plugin))
   Alpine.plugin(AsyncAlpine)
 }
 
